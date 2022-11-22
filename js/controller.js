55d2fb9bd7698bb41e9a62a37d1267fc66d3a095
julia.huettenmoser <julia.huettenmoser@ksb-sg.ch>
2022-11-22 16:25:10 +0100

    hello darkness my old friend

--- js/controller.js
+++ js/controller.js
@@ -17,7 +17,7 @@ function getInformation(amiibo) {
 }
 function getUsage(amiibo) {
     document.getElementById("usage").innerHTML = 
-    `<br><b><span class="usage">Usage</span></b></br>
+    `<br><b><span class="usage usage-title">Usage</span></b></br>
     <br><span class="usage" id="Switch">Switch</span></br>
     <br><span class="usage" id="WiiU>WiiU</div></br>
     <br><span class="usage" id="3DS">3DS</span></br>`;
